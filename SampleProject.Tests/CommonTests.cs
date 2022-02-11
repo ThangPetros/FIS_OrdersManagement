4e84819e2f13781e1c9a076f21bc0c3edf215215
ToanTa <toanta26042000@gmail.com>
2022-02-11 16:50:44 +0700

    Chỉnh sửa contructor CommonTest, tạo Init cho Test

--- SampleProject.Tests/CommonTests.cs
+++ SampleProject.Tests/CommonTests.cs
@@ -12,39 +12,51 @@ using TrueSight.Common;
 
 namespace SampleProject.Tests
 {
-    public class CommonTests
-    {
-        protected DataContext DataContext;
-        protected ServiceProvider provider;
-        public CommonTests()
-        {
-            _ = License.EfExtension;
+	public class CommonTests
+	{
+		protected DataContext DataContext;
+		protected ServiceProvider provider;
+		public CommonTests()
+		{
+			/*_ = License.EfExtension;
 
-            var config = new ConfigurationBuilder()
-               .AddJsonFile("appsettings.json")
-               .Build();
-            string connectionString = config.GetConnectionString("DataContext");
-            var options = new DbContextOptionsBuilder<DataContext>()
-                .UseSqlServer(connectionString)
-                .Options;
-            DataContext = new DataContext(options);
-            EntityFrameworkManager.ContextFactory = DbContext => new DataContext(options);
+			var config = new ConfigurationBuilder()
+			   .AddJsonFile("appsettings.json")
+			   .Build();
+			string connectionString = config.GetConnectionString("DataContext");
+			var options = new DbContextOptionsBuilder<DataContext>()
+			    .UseSqlServer(connectionString)
+			    .Options;
+			DataContext = new DataContext(options);
+			EntityFrameworkManager.ContextFactory = DbContext => new DataContext(options);*/
+		}
+		public void Init()
+		{
+			_ = License.EfExtension;
 
-        }
-
-        public async Task Clean()
-        {
-            DataContext.Database.ExecuteSqlRaw("EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL'");
-            DataContext.Database.ExecuteSqlRaw("EXEC sp_MSForEachTable 'SET QUOTED_IDENTIFIER ON; DELETE FROM ?'");
-            DataContext.Database.ExecuteSqlRaw("EXEC sp_MSForEachTable 'IF OBJECTPROPERTY(object_id(''?''), ''TableHasIdentity'') = 1 DBCC CHECKIDENT(''?'', RESEED, 0)'");
-            DataContext.Database.ExecuteSqlRaw("EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'");
-        }
-        public static Guid CreateGuid(string name)
-        {
-            MD5 md5 = MD5.Create();
-            Byte[] myStringBytes = ASCIIEncoding.Default.GetBytes(name);
-            Byte[] hash = md5.ComputeHash(myStringBytes);
-            return new Guid(hash);
-        }
-    }
+			var config = new ConfigurationBuilder()
+			   .AddJsonFile("appsettings.json")
+			   .Build();
+			string connectionString = config.GetConnectionString("DataContext");
+			var options = new DbContextOptionsBuilder<DataContext>()
+			    .UseSqlServer(connectionString)
+			    .Options;
+			DataContext = new DataContext(options);
+			EntityFrameworkManager.ContextFactory = DbContext => new DataContext(options);
+		}
+		public async Task Clean()
+		{
+			DataContext.Database.ExecuteSqlRaw("EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL'");
+			DataContext.Database.ExecuteSqlRaw("EXEC sp_MSForEachTable 'SET QUOTED_IDENTIFIER ON; DELETE FROM ?'");
+			DataContext.Database.ExecuteSqlRaw("EXEC sp_MSForEachTable 'IF OBJECTPROPERTY(object_id(''?''), ''TableHasIdentity'') = 1 DBCC CHECKIDENT(''?'', RESEED, 0)'");
+			DataContext.Database.ExecuteSqlRaw("EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'");
+		}
+		public static Guid CreateGuid(string name)
+		{
+			MD5 md5 = MD5.Create();
+			Byte[] myStringBytes = ASCIIEncoding.Default.GetBytes(name);
+			Byte[] hash = md5.ComputeHash(myStringBytes);
+			return new Guid(hash);
+		}
+	}
 }
